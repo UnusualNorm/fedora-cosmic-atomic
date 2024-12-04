4858ba77da2933a6de8b3b4da59a40232e7d5563
Unusual Norm <40722525+UnusualNorm@users.noreply.github.com>
2024-12-03 23:27:30 -0600

    Huh, why?

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -ouex pipefail
 
-# @cosmic-desktop
-rpm-ostree install cosmic-edit cosmic-files cosmic-session cosmic-store cosmic-term
+rpm-ostree install cosmic-session
+rpm-ostree install cosmic-files cosmic-term cosmic-store cosmic-edit
 
 systemctl enable cosmic-greeter.service
+
+mkdir -p /var/tmp
+chmod -R 1777 /var/tmp
